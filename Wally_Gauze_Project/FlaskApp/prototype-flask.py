d274e7ebab52ec590a3827bdf2889d06f5ed81f8
Wally <wallygauze@gmail.com>
2017-05-18 13:47:59 +0100

    modifications in app code

--- Wally_Gauze_Project/FlaskApp/prototype-flask.py
+++ Wally_Gauze_Project/FlaskApp/prototype-flask.py
@@ -10,14 +10,13 @@ df_whole.race.fillna(value='?', inplace=True)
 df_whole['#']=1
 
 # : Race profile for these groups
-# : # pre-Report code:
 
 df_whole = df_whole[
     (df_whole["race"] == "W")
     | (df_whole["race"] == "B")
     | (df_whole["race"] == "H")].copy()
 
-# : ## For the subsequent Chi-Square tests
+# :: For the subsequent Chi-Square tests
 
 t3_prop = df_whole.pivot_table(
     index='race',
@@ -37,19 +36,19 @@ def show_tables():
     total_positives = df_whole[
         (df_whole["armed"] == "unarmed")].shape[0]
 
-    line1 = "# of deceased unarmed: {}".format(df_whole[(df_whole["armed"] == "unarmed")].shape[0])
-    line2 = "% of deceased unarmed: {}".format(float(df_whole[(df_whole["armed"] == "unarmed")].shape[0]) / float(df_whole.shape[0]))
+    line1 = "count of killed that were unarmed: {}".format(df_whole[(df_whole["armed"] == "unarmed")].shape[0])
+    line2 = "prop. of total count that were unarmed: {0:.2f}".format(float(df_whole[(df_whole["armed"] == "unarmed")].shape[0]) / float(df_whole.shape[0]))
 
     data = df_whole[(df_whole["armed"] == "unarmed")]
     table1 = pd.DataFrame(data.pivot_table(index='race', values='#', aggfunc=[sum, lambda x:float(sum(x))/float(len(data.race))]))
     table1.rename(columns = {"sum":"count","<lambda>":"proportion"}, inplace = True)
-    table2_title = "Proportion with respect to whole race group in dataset"
+    table2_title = "Proportion of whole respective racial group in dataset"
     table2 = df_whole[(df_whole["armed"] == "unarmed")].race.fillna(
         value="?").value_counts()/ df_whole.race.fillna(value="?").value_counts().sort_values(ascending=False)
     table2 = pd.DataFrame(table2)
     table2 = pd.DataFrame(table2.race.rename('_'))
 
-    # : ### Chi-Square test
+    # :: Chi-Square test
     chisquare_result_ = chisquare(f_obs_2t3,
                                  f_exp=([x * total_positives for x in t3_group_proportions]))
     if chisquare_result_[1] < 0.01:
