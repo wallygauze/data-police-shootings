1731e68c21443177c9615ed21135a974615d5167
Wally <wallygauze@gmail.com>
2017-08-07 05:30:23 +0100

    latest work

--- Wally_Gauze_Project/FlaskApp/prototype-flask.py
+++ Wally_Gauze_Project/FlaskApp/prototype-flask.py
@@ -78,5 +78,49 @@ def show_tables():
                            x_result=chisquare_result,
                            pvalue_interpretation=pvalue_interpretation)
 
+@app.route("/wip") #seems like a better idea to use one function with data selection as input
+def show_tables():
+
+    total_positives = df_whole[
+        (df_whole["armed"] == "unarmed")].shape[0]
+
+    line1 = "count of people shot by the police that were unarmed: {}".format(df_whole[(df_whole["armed"] == "unarmed")].shape[0])
+    line2 = "prop. of total count that were unarmed: {0:.2f}".format(float(df_whole[(df_whole["armed"] == "unarmed")].shape[0]) / float(df_whole.shape[0]))
+
+    data = df_whole[(df_whole["armed"] == "unarmed")]
+    table1 = pd.DataFrame(data.pivot_table(index='race', values='#', aggfunc=[sum, lambda x:float(sum(x))/float(len(data.race))]))
+    table1.rename(columns = {"sum":"count","<lambda>":"proportion"}, inplace = True)
+    table2_title = "Proportion of whole respective racial group in dataset"
+    table2 = df_whole[(df_whole["armed"] == "unarmed")].race.fillna(
+        value="?").value_counts()/ df_whole.race.fillna(value="?").value_counts().sort_values(ascending=False)
+    table2 = pd.DataFrame(table2)
+    table2 = pd.DataFrame(table2.race.rename('_'))
+
+    # :: Chi-Square test
+    chisquare_result_ = chisquare(f_obs_2t3,
+                                 f_exp=([x * total_positives for x in t3_group_proportions]))
+    if chisquare_result_[1] < 0.01:
+        chisquare_result = "Chi-Square test of independence: Significant at alpha = 1%"
+    elif chisquare_result_[1] < 0.05:
+        chisquare_result = "Chi-Square test of independence: Significant at alpha = 5%"
+    elif chisquare_result_[1] < 0.1:
+        chisquare_result = "Chi-Square test of independence: Significant at alpha = 10%"
+    else:
+        chisquare_result = "Chi-Square test of independence: Not significant at even alpha = 10%"
+
+    pvalue_interpretation = """(This percentage can be broadly interpreted as
+    showing the probability such a result would have been found if there was
+    actually no correlation between race and the proportion of the whole
+    respective racial group in the dataset that falls in the \'unarmed\'
+    group.)"""
+
+    return render_template('view.html',
+                           lines=[line1, line2],
+                           table=table1.to_html(),
+                           table2_title=table2_title,
+                           table2=table2.to_html(),
+                           x_result=chisquare_result,
+                           pvalue_interpretation=pvalue_interpretation)
+
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
